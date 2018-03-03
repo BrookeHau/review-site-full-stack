cae384b306a8e7de6712d2a040ac9b47e6205d8e
BrookeHau <brookerhauser@gmail.com>
2018-03-03 10:42:52 -0500

    A few reviews added

--- src/main/java/org/wecancodeit/reviewsitefullstack/ReviewPopulator.java
+++ src/main/java/org/wecancodeit/reviewsitefullstack/ReviewPopulator.java
@@ -29,11 +29,11 @@ public class ReviewPopulator implements CommandLineRunner {
 		category4 = catRepo.save(category4);
 		Tag tag1 = new Tag("high cost");
 		tag1 = tagRepo.save(tag1);
-		Review review = new Review("Yoga1", "Great time", category1,  tag1);
+		Review review = new Review("Yoga Six", "Yoga Six has a studio in Upper Arlington.  It has a variety of class types such as Hot Yoga, Slow Flow, Power Yoga, and Performance.", category1,  tag1);
 		review = reviewRepo.save(review);
-		Review review2 = new Review("Yoga2", "excellent", category1, tag1);
+		Review review2 = new Review("CorePower Yoga", "CorePower has an Easton and Short North Location.  Benefits of this studio include: membership is valid at both locations, all classes are hot (>80 degrees), and a variation of classes from recovery-style classes to more intense scuplting ones. ", category1, tag1);
 		review2 = reviewRepo.save(review2);
-		Review review3 = new Review("Barre", "excellent", category2, tag1);
+		Review review3 = new Review("Barre3", "Barre3 is an amazing workout! The workout incorporates cardio and weights throughout the workout to give you a total body workout. \\n Not only that, but it targets muscles you never knew you had.  Unlike a lot of workouts, barre3 has modifications for every move, so if you have a bad knee or back there will be a modification to alleviate the pain and make each posture work for you. I've never been in better shape, and a lot of that has to do with barre3.", category2, tag1);
 		review3 = reviewRepo.save(review3);
 		Review review4 = new Review("Pilates", "excellent", category3, tag1);
 		review4 = reviewRepo.save(review4);
