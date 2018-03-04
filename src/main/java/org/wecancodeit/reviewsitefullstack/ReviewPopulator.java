131fd949095bdf614d692f88e097c0242f006d1d
BrookeHau <brookerhauser@gmail.com>
2018-03-04 17:51:25 -0500

    updated reviews

--- src/main/java/org/wecancodeit/reviewsitefullstack/ReviewPopulator.java
+++ src/main/java/org/wecancodeit/reviewsitefullstack/ReviewPopulator.java
@@ -35,9 +35,9 @@ public class ReviewPopulator implements CommandLineRunner {
 		review2 = reviewRepo.save(review2);
 		Review review3 = new Review("Barre3", "Barre3 is an amazing workout! The workout incorporates cardio and weights throughout the workout to give you a total body workout. \\n Not only that, but it targets muscles you never knew you had.  Unlike a lot of workouts, barre3 has modifications for every move, so if you have a bad knee or back there will be a modification to alleviate the pain and make each posture work for you. I've never been in better shape, and a lot of that has to do with barre3.", category2, tag1);
 		review3 = reviewRepo.save(review3);
-		Review review4 = new Review("Pilates", "excellent", category3, tag1);
+		Review review4 = new Review("The Butcher Shop", "Similar to barre, this workout targets muscles you've never worked before. And a big reason for that is due to the machine's design. Each workout is 50 minutes (or 40 if you are doing the ab class), and consists of slow, controlled movements. This workout is so hard that you will need to take a break, and I don't mean the classic break during a workout where it's mostly in your head. I mean these moves are so challenging and target muscles you rarely use that you will need to take breaks. However, as I've continued to go, I've noticed I'm getting stronger, and am taking fewer breaks.", category3, tag1);
 		review4 = reviewRepo.save(review4);
-		Review review5 = new Review("SOS", "excellent", category4, tag1);
+		Review review5 = new Review("SOS", "This workout consists of a variety of workouts, from yoga to HIIT workouts.  The atmosphere and community behind this gym is amazing.", category4, tag1);
 		review5 = reviewRepo.save(review5);
 	}
 }
