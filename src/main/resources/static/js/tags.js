67c3f3a2d89053dbf344445bf4b0644ec0093c74
BrookeHau <brookerhauser@gmail.com>
2018-03-19 07:34:46 -0400

    added ajax

---
+++ src/main/resources/static/js/tags.js
@@ -0,0 +1,57 @@
+const xhr = new XMLHttpRequest()
+
+xhr.onreadystatechange = function() {
+	if (xhr.readyState === 4 && xhr.status === 200) {
+
+		const res = xhr.responseText;
+
+		const tagList = document.getElementByClass('tagList');
+
+		tagList.innerHTML = res;
+
+	}
+}
+
+function clearField() {
+
+	document.newTagForm.reset();
+
+}
+
+const form = document.getElementById('newTagForm');
+
+
+
+	const reviewTagId = document.getElementById('reviewTagId').value;
+	const descriptionId = document.getElementById('descriptionId').value;
+
+	xhr.open('POST', 'http://localhost:8080/add-tag', true);
+	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhr.send("reviewId=" + reviewTagId + "&tagDescription=" + descriptionId);
+
+	
+	
+
+})
+//const newTagFiller = document.getElementById('descriptionId');
+//newTagFiller.style.placeholder="this";
+
+// creating ajax call to delete tag by method used created in Controller
+const removeForm = document.getElementById('deleteTagForm');
+removeForm.addEventListener('submit', function(event) {
+	event.preventDefault();
+	
+	const tagDeleteBox = document.getElementById('removeTagDescriptionId').value;
+	if (tagDeleteBox == "") {
+		alert("Please fill in tag description!")
+		return false;
+	}
+	
+	const removeTagId = document.getElementById('removeTagId').value;
+	const removeTagDescriptionId = document.getElementById('removeTagDescriptionId').value;
+	
+	xhr.open('POST', 'http://localhost:8080/remove-tag', true);
+	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhr.send("reviewId=" + removeTagId + "&tagDescription="	+ removeTagDescriptionId);
+
+})
